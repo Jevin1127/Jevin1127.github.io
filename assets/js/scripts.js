6d8cd0ebf3e21fc545e1f588c7b7bb7b9697d747
Jevin Lopez <jevinvega@gmail.com>
2025-08-09 22:51:23 -0600

    Cambio otra vez nombre jpg y añadi otra imagen 16

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 const genericChordImages = {
-    guitar: 'https://jevin1127.github.io/assets/img/guitar/guitar.png', 
+    guitar: 'https://jevin1127.github.io/assets/img/guitar/guitar.webp', 
     piano: 'assets/img/piano/32407b370080d1d7699b97799c01a300-doodle-de-instrumentos-musicales-para-piano.webp',
     bass: 'assets/img/bass/6196550.png'
 };
